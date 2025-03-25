8092ef7357e46a52070a1f9651308cb998155165
HamCam203 <camarahamoudia@gmail.com>
2025-03-25 14:20:31 +0100

    Initial commit

---
+++ src/main/java/com/backend/BlogData/BlogDataApplication.java
@@ -0,0 +1,13 @@
+package com.backend.BlogData;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogDataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogDataApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/backend/BlogData/BlogDataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.BlogData;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogDataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
